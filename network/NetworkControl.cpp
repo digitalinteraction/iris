77abba39c2814e0093329104052362189212caf7
Tobias Bruehwiler <tobias.bruehwiler@ncl.ac.uk>
2016-07-08 17:48:09 +0100

    qt gui!

--- network/NetworkControl.cpp
+++ network/NetworkControl.cpp
@@ -54,7 +54,7 @@ void NetworkControl::run(){
     unsigned long buildtopo = currenttime +3000;
     
     while (true) {
-        //printf("nc running start\n");
+        printf("nc running start\n");
         FD_ZERO(&readfs);
         clock_gettime(CLOCK_REALTIME, &current);
         currenttime = current.tv_sec*1000 + current.tv_nsec/1000000;
@@ -85,23 +85,23 @@ void NetworkControl::run(){
                 }
             }
         }
-        //printf("A\n");
+        printf("A\n");
         if(currenttime > nextcheck){
             rel->check_timeouts();
             nextcheck = currenttime + 20;
         }
-        //printf("B\n");
+        printf("B\n");
         if(currenttime > toposend){
             toposend = currenttime + 10;
             topo->send();
         }
-        //printf("C\n");
+        printf("C\n");
         if(currenttime > buildtopo){
             topo->build_mapping();
             buildtopo = currenttime + 2456;
 
         }
-       //printf("D\n");
+       printf("D\n");
 #ifdef CLIENT_SIDE
         if(currenttime > nextprint){
             struct mallinfo mi = mallinfo();
