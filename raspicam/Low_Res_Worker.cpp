ece98e5878cd880d2f645a3706821d86b76fdc99
Tobias Bruehwiler <tobias.bruehwiler@ncl.ac.uk>
2016-07-21 16:37:34 +0100

    trying to add contour matching

--- raspicam/Low_Res_Worker.cpp
+++ raspicam/Low_Res_Worker.cpp
@@ -334,7 +334,7 @@ uint8_t Low_Res_Worker::match_contours(vector<Point> *contour, uint8_t run) {
         }
 
         if (res < 0.1) {
-            printf("%d::found match with similarity %f\n", run, res);
+            printf("%d::found match with similarity %f and id %d\n", run, res, found->id);
             return 1;
         }
 
