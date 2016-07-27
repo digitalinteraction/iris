e6427df1b4e361b853ff64b80a13952accc5bcb7
Tobias Bruehwiler <tobias.bruehwiler@ncl.ac.uk>
2016-07-27 14:25:21 +0100

    a

--- raspicam/Low_Res_Worker.cpp
+++ raspicam/Low_Res_Worker.cpp
@@ -45,7 +45,7 @@ Low_Res_Worker::Low_Res_Worker(Packetbuffer *out, NetworkControl *nc, Buffer *im
     this->first = 0;
     this->last = 0;
     
-    pMOG2 = createBackgroundSubtractorMOG2();
+    pMOG2 = createBackgroundSubtractorGMG();
 
 }
 
@@ -79,7 +79,7 @@ void Low_Res_Worker::process_image(uint8_t *image, size_t image_size) {
         split(hsv, channel);
         //threshold(channel[1], mask, 40, 255, THRESH_BINARY);
         
-        pMOG2->apply(img, mask, 0.001);
+        pMOG2->apply(img, mask);
         //CLEANING UP////////////////////////////////////////
         Mat kernel = Mat::ones(3, 3, CV_8U);
         Mat cleaned;
