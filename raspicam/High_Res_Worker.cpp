35aae84c5590c418dcfc9ef4930fa1b8c9ba6709
Tobias Bruehwiler <tobias.bruehwiler@ncl.ac.uk>
2016-07-29 15:29:43 +0100

    first test communication

--- raspicam/High_Res_Worker.cpp
+++ raspicam/High_Res_Worker.cpp
@@ -260,6 +260,8 @@ void High_Res_Worker::find_features(RASPITEX_PATCH *patch, uint8_t group) {
         item->id = comm->file_cnt;
         deb_printf("size of item: %d %d\n", sizeof(patch_packet), sizeof(feature_vector));
         deb_printf("item %p\n", item);
+        printf("size of item: %d %d\n", sizeof(patch_packet), sizeof(feature_vector));
+        printf("item %p\n", item);
         comm->ask_neighbours(item);
 
         //fill item with data from RASPIPATCH
