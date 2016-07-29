fd84a2d2783fee50a95a9a551d2e580f366557ee
Tobias Bruehwiler <tobias.bruehwiler@ncl.ac.uk>
2016-07-29 17:31:14 +0100

    first test communication

--- raspicam/CommImage.cpp
+++ raspicam/CommImage.cpp
@@ -48,7 +48,7 @@ CommImage::~CommImage() {
 void CommImage::save_to_file_image(Mat *pic){
     deb_printf("save_to_file_image: pic %d\n", pic->total());
     char buf_pic[100];
-    snprintf(buf_pic, 100, "patches/%lx%d.png", nc->topo->mac, file_cnt);
+    snprintf(buf_pic, 100, "patches/%llx_%d.png", nc->topo->mac, file_cnt);
     deb_printf("Filename: %s\n", buf_pic);
     imwrite(buf_pic, *pic);
 }
