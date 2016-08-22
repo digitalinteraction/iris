216d3efbd13a37ca95c93360d9be077cdfa44010
Tobias Bruehwiler <tobias.bruehwiler@ncl.ac.uk>
2016-08-22 10:40:31 +0100

    a

--- raspicam/High_Res_Worker.cpp
+++ raspicam/High_Res_Worker.cpp
@@ -61,7 +61,7 @@ High_Res_Worker::High_Res_Worker(Buffer *buffer, Packetbuffer *out_buf, Packetbu
         printf("classifier loaded\n");
     }
     
-    surf = xfeatures2d::SURF::create( 100 );
+    surf = xfeatures2d::SURF::create( 50 );
 }
 
 High_Res_Worker::~High_Res_Worker() {
